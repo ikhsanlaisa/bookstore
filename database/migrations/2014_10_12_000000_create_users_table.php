a1df29a35875b1fb2b59d162e039b00949fa1d76
ikhsan laisa <ikhsanlaisa96@gmail.com>
2018-11-30 00:51:52 +0700

    update db

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,9 +16,10 @@ class CreateUsersTable extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->increments('id');
             $table->string('name');
+            $table->string("username")->unique();
             $table->string('email')->unique();
             $table->string('password');
-            $table->text('roles')->nullable();
+            $table->string('roles')->nullable();
             $table->text('address')->nullable();
             $table->integer('city_id')->nullable();
             $table->integer('province_id')->nullable();
@@ -27,6 +28,13 @@ class CreateUsersTable extends Migration
             $table->enum('status', ['ACTIVE', 'INACTIVE'])->default('ACTIVE');
             $table->rememberToken();
             $table->timestamps();
+
+            $table->string("username")->unique();
+            $table->string("roles");
+            $table->text("address")->nullable();
+            $table->string("phone")->nullable();
+            $table->string("avatar")->nullable();
+            $table->enum("status", ["ACTIVE", "INACTIVE"]);
         });
     }
 
